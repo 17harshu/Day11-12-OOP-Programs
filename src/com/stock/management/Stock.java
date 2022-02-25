c21140809133307e0f5dbccd00f306682f05c0c2
17harshu <harshalanandas17@gmail.com>
2022-02-25 20:48:14 +0530

    added: stock management report

---
+++ src/com/stock/management/Stock.java
@@ -0,0 +1,43 @@
+package com.stock.management;
+
+public class Stock {
+	private String stockNames;
+	private Double numOfShare;
+	private Double sharePrice;
+
+	public Stock() {
+		super();
+	}
+
+	// Parameterized constructor
+	public Stock(String stockNames, Double numOfShare, Double sharePrice) {
+		super();
+		this.stockNames = stockNames;
+		this.numOfShare = numOfShare;
+		this.sharePrice = sharePrice;
+	}
+
+	public String getStockNames() {
+		return stockNames;
+	}
+
+	public void setStockNames(String stockNames) {
+		this.stockNames = stockNames;
+	}
+
+	public Double getNumOfShare() {
+		return numOfShare;
+	}
+
+	public void setNumOfShare(Double numOfShare) {
+		this.numOfShare = numOfShare;
+	}
+
+	public Double getSharePrice() {
+		return sharePrice;
+	}
+
+	public void setSharePrice(Double sharePrice) {
+		this.sharePrice = sharePrice;
+	}
+}
